016566e947df5e6948f608852db41cf0edaab6f8
Шлома Сергей Николаевич <-@534school.spb.ru>
2023-10-27 09:35:36 +0300

    Added code in Example012_Methods

--- Example012_Methods/Program.cs
+++ Example012_Methods/Program.cs
@@ -24,6 +24,8 @@
 // newText = Replace(text, 'к', 'К');
 // Console.WriteLine(newText);
 
+using System.ComponentModel.DataAnnotations;
+
 int[] arr = {1, 5, 4, 3, 2, 6, 7, 1, 1};
 
 void PrintArray(int[] array)
@@ -39,10 +41,14 @@ void PrintArray(int[] array)
 
 void SelectionSort(int[] array)
 {
-    for (int i = 0; i < array.Length; i++)
+    for (int i = 0; i < array.Length - 1; i++)
     {
         int minPosition = i;
 
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if(array[j] < array[minPosition]) minPosition = j;
+        }
 
 
         int temporary = array[i];
@@ -51,4 +57,6 @@ void SelectionSort(int[] array)
     }
 }
 
+PrintArray(arr);
+SelectionSort(arr);
 PrintArray(arr);
